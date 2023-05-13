21def1c72803943726c399683cf357e3af7d0b8e
george <7jagjag@gmail.com>
2023-05-13 20:08:20 +0200

    init

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = `CodeCraft: Exploring the React, TypeScript, Java, and Rust Journey`;
+export const SITE_DESCRIPTION = 'CodeCraft: Exploring the React, TypeScript, Java, and Rust Journey';
